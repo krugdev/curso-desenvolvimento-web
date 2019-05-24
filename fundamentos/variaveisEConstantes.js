5c05de138d6ae8f54c17f3dce1f6c07f7b18276b
krugdev <andrekrueger.ene@gmail.com>
2019-05-24 10:01:36 -0300

    blob

---
+++ fundamentos/variaveisEConstantes.js
@@ -0,0 +1,18 @@
+var a = 3
+let b = 4
+
+console.log(a,b)
+
+var a = 30
+b = 40 // let b = 40 dá erro pois o javascript não permite redeclarar uma variável let. Por esse motivo deve-se preferir o uso do let em relação ao var, que o javascript não checa!
+
+console.log(a,b)
+
+a = 300
+b = 400
+
+console.log(a,b)
+
+const c = 5
+
+console.log(c)
